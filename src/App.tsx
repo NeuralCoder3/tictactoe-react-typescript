51bd56d742c277b0dd6fb4b9dc4322cadd2bd108
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2022-06-23 15:08:05 +0200

    for loop creation

--- src/App.tsx
+++ src/App.tsx
@@ -58,24 +58,18 @@ class Board extends React.Component<{},BoardState> {
       status = `Next player: ${getPlayer(this.state.steps)}`;
     }
 
+    const rows = [0,1,2].map((i) => {
+      return <div className="board-row">
+        {[0,1,2].map((j) => 
+          this.renderSquare(i * 3 + j)
+        )}
+      </div>
+    });
+
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
